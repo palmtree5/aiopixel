a81859334fbfd97b7269b254ec4f58e9786b9a09
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2018-05-08 16:18:54 -0800

    Centralize request making + add docstrings

--- aiopixel/client.py
+++ aiopixel/client.py
@@ -25,142 +25,311 @@ class PixelClient:
         self._session = aiohttp.ClientSession()
         self._achievements = PixelAchievements()
 
-    async def _request(self, route: str, params: dict=None):
-        pass
-
-    async def boosters(self) -> List[Booster]:
-        params = {"key": self._api_key}
-        async with self._session.get(BASE_API_URL.format("/boosters"), params=params) as r:
+    async def _request(self, route: str, method: str, params: dict=None):
+        async with self._session.request(method, BASE_API_URL.format(route), params=params) as r:
             if not r.status == 200:
                 raise PixelException(
-                    "Error occurred. "
-                    "Status code: {0.status}. "
+                    "An error occurred while making the request.\n"
+                    "Status code: {0.status}.\n"
                     "Reason: {0.reason}".format(r)
                 )
             data = await r.json()
-        if not data["success"]:
-            if data["cause"] == "Invalid API key!":
-                raise InvalidKeyException(data["cause"])
+            if not data["success"]:
+                if data["cause"] == "Invalid API key!":
+                    raise InvalidKeyException(data["cause"])
+                else:
+                    raise PixelException("Error: {}".format(data["cause"]))
             else:
-                raise PixelException("Error: {}".format(data["cause"]))
-        else:
-            raw_boosters = data["boosters"]
-            boosters = []
-            for r_b in raw_boosters:
-                boosters.append(Booster(
-                    r_b["_id"], r_b["purchaserUuid"], r_b["amount"],
-                    r_b["originalLength"], r_b["length"], r_b["gameType"],
-                    r_b["dateActivated"], stacked=r_b.get("stacked", None)
-                ))
-            return boosters
+                return data
+
+    async def boosters(self) -> List[Booster]:
+        """
+        Get a list of all boosters
+
+        Returns
+        -------
+        `list` of `Booster`
+            A list of all boosters in the queue
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        """
+        data = await self._request(
+            "/boosters", "GET", params={"key": self._api_key}
+        )
+        raw_boosters = data["boosters"]
+        boosters = []
+        for r_b in raw_boosters:
+            boosters.append(Booster(
+                r_b["_id"], r_b["purchaserUuid"], r_b["amount"],
+                r_b["originalLength"], r_b["length"], r_b["gameType"],
+                r_b["dateActivated"], stacked=r_b.get("stacked", None)
+            ))
+        return boosters
 
     async def find_guild_by_name(self, name: str) -> str:
-        params = {
-            "key": self._api_key,
-            "byName": name
-        }
-        async with self._session.get(BASE_API_URL.format("/findGuild"), params=params) as r:
-            data = await r.json()
-        if data["success"]:
-            if data["guild"] is None:
-                raise GuildNotFound()
-            return data["guild"]
+        """
+        Get the guild id for a guild with the specified name
+
+        Parameters
+        ----------
+        name: str
+            The guild name
+
+        Returns
+        -------
+        str
+            The guild id
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        GuildNotFound
+            If no guild is found
+        """
+        data = await self._request(
+            "/findGuild", "GET", 
+            params={"key": self._api_key, "byName": name}
+        )
+        if data["guild"] is None:
+            raise GuildNotFound()
+        return data["guild"]
 
     async def find_guild_by_uuid(self, uuid: str) -> str:
-        params = {
-            "key": self._api_key,
-            "byUuid": clean_uuid(uuid)
-        }
-        async with self._session.get(BASE_API_URL.format("/findGuild"), params=params) as r:
-            data = await r.json()
-        if data["success"]:
-            if data["guild"] is None:
-                raise PlayerNotInGuild()
-            return data["guild"]
+        """
+        Get the guild id for the specified player's guild
+
+        Parameters
+        ----------
+        uuid: str
+            The player's uuid
+
+        Returns
+        -------
+        str
+            The guild id
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        PlayerNotInGuild
+            If no guild is found
+        """
+        data = await self._request(
+            "/findGuild", "GET", 
+            params={"key": self._api_key, "byUuid": clean_uuid(uuid)}
+        )
+        if data["guild"] is None:
+            raise PlayerNotInGuild()
+        return data["guild"]
 
     async def friends(self, uuid: str) -> List[Friend]:
-        params = {
-            "key": self._api_key,
-            "uuid": clean_uuid(uuid)
-        }
-        async with self._session.get(BASE_API_URL.format("/friends"), params=params) as r:
-            data = await r.json()
+        """
+        Get friends of a player
 
-        if data["success"]:
-            friends_list = []
-            for raw_friend in data["records"]:
-                friends_list.append(
-                    Friend(
-                        raw_friend["_id"],
-                        raw_friend["uuidSender"],
-                        raw_friend["uuidReceiver"],
-                        raw_friend["started"]
-                    )
+        Parameters
+        ----------
+        uuid: str
+            The uuid of the player to get friends of
+        
+        Returns
+        -------
+        `list` of `Friend`
+            A list of objects representing the player's friendships
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or if an error is returned in the response json
+        """
+        data = await self._request(
+            "/friends", "GET", 
+            params={"key": self._api_key, "uuid": clean_uuid(uuid)}
+        )
+
+        friends_list = []
+        for raw_friend in data["records"]:
+            friends_list.append(
+                Friend(
+                    raw_friend["_id"],
+                    raw_friend["uuidSender"],
+                    raw_friend["uuidReceiver"],
+                    raw_friend["started"]
                 )
-            return friends_list
+            )
+        return friends_list
 
     async def guild(self, guild_id: str) -> Guild:
-        params = {
-            "key": self._api_key,
-            "id": guild_id
-        }
-        async with self._session.get(BASE_API_URL.format("/guild"), params=params) as r:
-            data = await r.json()
-        if data["success"]:
-            if data["guild"] is None:
-                raise GuildNotFound()
-            guild = data["guild"]
-            members = []
-            for m in guild["members"]:
-                members.append(GuildMember(**m))
-            tag = GuildTag(guild["tag"], guild["tagColor"])
-            banner = GuildBanner(
-                guild["banner"]["Base"], guild["banner"]["Patterns"]
-            )
-            return Guild(
-                guild["_id"], guild["bankSizeLevel"], guild["canMotd"],
-                guild["canParty"], guild["canTag"], guild["coins"],
-                guild["coinsEver"], guild["created"], guild["joinable"],
-                guild["memberSizeLevel"], members, guild["name"], tag,
-                banner, guild["vipCount"], guild["mvpCount"]
-            )
+        """
+        Get a guild
+
+        Parameters
+        ----------
+        guild_id: str
+            The id of the guild to look up
+        
+        Returns
+        -------
+        Guild
+            The requested guild
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        GuildNotFound
+            If no guild is found
+        """
+        data = await self._request(
+            "/guild", "GET", 
+            params={"key": self._api_key, "id": guild_id}
+        )
+
+        if data["guild"] is None:
+            raise GuildNotFound()
+        guild = data["guild"]
+        members = []
+        for m in guild["members"]:
+            members.append(GuildMember(m))
+
+        tag = GuildTag(guild.get("tag", None), guild.get("tagColor", None))
+        guild_can_motd = guild.get("canMotd", False)
+        guild_can_party = guild.get("canParty", False)
+        guild_can_tag = guild.get("canTag", False)
+        guild_joinable = guild.get("joinable", False)
+        vip_count = guild.get("vipCount", 0)
+        mvp_count = guild.get("mvpCount", 0)
+        banner = GuildBanner(
+            guild["banner"]["Base"], guild["banner"]["Patterns"]
+        )
+        return Guild(
+            guild["_id"], guild["bankSizeLevel"], guild_can_motd,
+            guild_can_party, guild_can_tag, guild["coins"],
+            guild["coinsEver"], guild["created"], guild_joinable,
+            guild["memberSizeLevel"], members, guild["name"], tag,
+            banner, vip_count, mvp_count
+        )
 
     async def leaderboards(self) -> List[Leaderboard]:
-        params = {
-            "key": self._api_key
-        }
-        async with self._session.get(BASE_API_URL.format("/leaderboards"), params=params) as r:
-            data = await r.json()
-        if data["success"]:
-            raw_leaderboards = data["leaderboards"]
-            leaderboard_list = []
-            for game, lbs in raw_leaderboards.items():
-                game_type = getattr(GameType, game)
-                for lb in lbs:
-                    leaders = []
-                    for l in lb["leaders"]:
-                        leaders.append(LeaderboardMember(l))
-                    leaderboard_list.append(
-                        Leaderboard(
-                            lb["path"], lb["prefix"], lb["count"],
-                            leaders, lb["title"], game_type
-                        )
+        """
+        Get all leaderboards
+        
+        Returns
+        -------
+        `list` of `Leaderboard`
+            A list of all leaderboards
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        """
+        data = await self._request(
+            "/leaderboards", "GET", 
+            params={"key": self._api_key}
+        )
+
+        raw_leaderboards = data["leaderboards"]
+        leaderboard_list = []
+        for game, lbs in raw_leaderboards.items():
+            game_type = getattr(GameType, game)
+            for lb in lbs:
+                leaders = []
+                for l in lb["leaders"]:
+                    leaders.append(LeaderboardMember(l))
+                leaderboard_list.append(
+                    Leaderboard(
+                        lb["path"], lb["prefix"], lb["count"],
+                        leaders, lb["title"], game_type
                     )
-            return leaderboard_list
+                )
+        return leaderboard_list
 
     async def player_from_name(self, name: str) -> Player:
+        """
+        Gets the player represented by the specified name
+
+        Parameters
+        ----------
+        name: str
+            The name of the player
+        
+        Returns
+        -------
+        Player
+            The specified player
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        PlayerNotFound
+            If the player doesn't exist or has never logged into the server
+        """
         uuid = await get_player_uuid(name, self._session)
         if uuid is None:
             raise PlayerNotFound()
         return await self.player_from_uuid(uuid)
 
     async def player_from_uuid(self, uuid: str) -> Player:
-        params = {
-            "key": self._api_key,
-            "uuid": uuid
-        }
-        async with self._session.get(BASE_API_URL.format("/player"), params=params) as r:
-            data = await r.json()
+        """
+        Gets the player represented by the specified uuid
+
+        Parameters
+        ----------
+        uuid: str
+            The uuid of the player
+        
+        Returns
+        -------
+        Player
+            The specified player
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        PlayerNotFound
+            If the player doesn't exist or has never logged into the server
+        """
+        data = await self._request(
+            "/player", "GET", 
+            params={"key": self._api_key, "uuid": uuid}
+        )
+
         if not bool(data["player"]):
             raise PlayerNotFound()
         player_data = data["player"]
@@ -180,12 +349,35 @@ class PixelClient:
         )
     
     async def session(self, uuid: str) -> PlayerSession:
-        params = {
-            "key": self._api_key,
-            "uuid": uuid
-        }
-        async with self._session.get(BASE_API_URL.format("/session"), params=params) as r:
-            data = await r.json()
+        """
+        Get a player's current session
+
+        Parameters
+        ----------
+        uuid: str
+            The player to get a session for
+        
+        Returns
+        -------
+        PlayerSession
+            The player's current session
+        
+        Raises
+        ------
+        InvalidKeyException
+            If the client's API key is not valid
+        
+        PixelException
+            If an error occurs while making the request or is present in the response json
+        
+        NoSessionForPlayer
+            If the specified player does not have a session
+        """
+        data = await self._request(
+            "/session", "GET", 
+            params={"key": self._api_key, "uuid": uuid}
+        )
+
         if not data["session"]:
             raise NoSessionForPlayer()
         else:

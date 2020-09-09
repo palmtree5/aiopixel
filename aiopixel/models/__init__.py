2f0b0533bbfcce46e956ff89879528770bc24c2d
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2020-09-08 20:51:20 -0800

    0.2.6 release

--- aiopixel/models/__init__.py
+++ aiopixel/models/__init__.py
@@ -4,5 +4,5 @@ from .guilds import GuildMember, GuildTag, Guild
 from .leaderboards import Leaderboard, LeaderboardMember
 from .players import Player, PlayerAchievements, PlayerRank, \
     PixelAchievements, TieredAchievement, AchievementTier, OneTimeAchievement
-from .sessions import PlayerSession
+from .sessions import PlayerStatus
 from .stats import *
